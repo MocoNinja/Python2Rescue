4d21fe7efc24847340904636c67a9e47abeb815d
MocoNinja <javiergonzaleztello@gmail.com>
2017-04-21 18:00:51 +0200

    Terminada la parte hablada en clase
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- getData.py
+++ getData.py
@@ -3,25 +3,27 @@
 import sys
 import csv
 
-def acortar(palabra, 13):
-	return line[13:]
+def acortar(palabra, patron):
+    caracteresBorrar = len(patron) + 2 # el patron lleva pegados ; y dos espacios de forma fija
+    return palabra[caracteresBorrar:]
+
+
 nombreArchivo = sys.argv.__getitem__(1)
 
 etiquetas = ['MatCodig', 'MatNumSe', 'MatNumMe', 'MatNumCo', 'MatCant', 'MatTipo', 'MatDescr', 'MatMarca', 'MatLocal', '$UpdatedBy', '$Revisions', 'Observaciones']
 print("Leyendo datos del archivo: ", nombreArchivo)
 camposEsperados = 12
 
-# quitar caracteres palabra + 1 + 2
 
 with open('eggs.csv', 'w', newline='') as csvfile:
-	spamwriter = csv.writer(csvfile, delimiter=' ', quotechar=';', quoting=csv.QUOTE_MINIMAL)
-	with open(nombreArchivo, 'r') as archivoAbierto:
-		numeroActual = "1"
-		for line in archivoAbierto:
-			lineaActual = ""
-			lineaActual += archivoAbierto.readline()
-			for i in etiquetas:
-				if i in lineaActual:
-					spamwriter.writerow("#Fichero" + numeroActual)
-					spamwriter.writerow(i + lineaActual)
-			#spamwriter.writerow(etiquetas.__getitem__(0) + archivoAbierto.readline())
+    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar=';', quoting=csv.QUOTE_MINIMAL)
+    with open(nombreArchivo, 'r') as archivoAbierto:
+        numeroActual = 1
+        for line in archivoAbierto:
+            lineaActual = ""
+            lineaActual += archivoAbierto.readline()
+            for i in etiquetas:
+                if i in lineaActual:
+                    textoTratado = acortar(lineaActual, i)
+                    spamwriter.writerow("#Fichero" + numeroActual)
+                    spamwriter.writerow(i + textoTratado)
