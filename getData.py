f28efa3be6581f1ab874e2421f50dff363a00f7c
MocoNinja <javiergonzaleztello@gmail.com>
2017-04-22 19:23:35 +0200

    Reorganizado
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- getData.py
+++ getData.py
@@ -1,29 +1,34 @@
-#! /usr/bin/python3
-
 import sys
-import csv
-
-def acortar(palabra, patron):
-    caracteresBorrar = len(patron) + 2 # el patron lleva pegados : y dos espacios de forma fija
-    return palabra[caracteresBorrar:]
-
 
 nombreArchivo = sys.argv.__getitem__(1)
+etiquetas = ['MatCodig:  ', 'MatNumSe:  ', 'MatNumMe:  ', 'MatNumCo:  ', 'MatCant:  ', 'MatTipo:  ', 'MatDescr:  ', 'MatMarca:  ', 'MatLocal:  ', '$UpdatedBy:  ', '$Revisions:  ']
+# Cambiar estas etiquetas por las pertinentes según lo hablado con Merche
+alias = ['Et1:','Et2:','Et3:','Et4:','Et5:','Et6:','Et7','Et8:','Et9:','Et10:','Et11:']
 
-etiquetas = ['MatCodig', 'MatNumSe', 'MatNumMe', 'MatNumCo', 'MatCant', 'MatTipo', 'MatDescr', 'MatMarca', 'MatLocal', '$UpdatedBy', '$Revisions', 'Observaciones']
-print("Leyendo datos del archivo: ", nombreArchivo)
-camposEsperados = len(etiquetas)
+def crearArchivo(nombre):
+    nombre += ".txt"
+    archivoDestino = open(nombre, 'w')
+    archivoDestino.write("## File successfully created!\n\n")
+    archivoDestino.close()
+def escribirLinea(linea):
+    linea += "\n"
+    archivoDestino = open(nombreArhivo + '.txt','a')
+    archivoDestino.write(linea)
+    archivoDestino.close()
+nombreArhivo = "prueba"
+crearArchivo(nombreArhivo)
 
 
-with open('output.txt', 'w') as archivoSalida:
-    with open(nombreArchivo, 'r') as archivoAbierto:
-        campoActual = 1
-        for line in archivoAbierto:
-            lineaActual = ""
-            lineaActual += archivoAbierto.readline()
+archivoOrigen = open(nombreArchivo, 'r')
+for line in archivoOrigen:
+    resultado = ""
+    for i in range (0, len(etiquetas)):
+        lineaLeida = line.strip('\n')
+        if not (lineaLeida == '\n'):
             for i in etiquetas:
-                if i in lineaActual:
-                    textoTratado = acortar(lineaActual, i)
-                    archivoSalida.write("#Fichero" + str(campoActual))
-                    campoActual += 1
-                    archivoSalida(i + textoTratado)
+                if i in lineaLeida:
+                    resultado += alias[etiquetas.index(i)]
+                    resultado += lineaLeida.strip(i)
+                    resultado +=';'
+            line = next(archivoOrigen)
+    escribirLinea(resultado)
