ebf44a56c5be3fc45c7a939ffe99a3e929ecff20
MocoNinja <javiergonzaleztello@gmail.com>
2017-04-24 16:31:39 +0200

    Mejorada la entrada

--- getData.py
+++ getData.py
@@ -1,24 +1,34 @@
-import sys
+from sys import argv
 
-nombreArchivo = sys.argv.__getitem__(1)
+nombreArchivo = argv.__getitem__(1)
+if len(argv) == 3:
+    nombreArchivoDestino = argv.__getitem__(2)
+    if nombreArchivoDestino.find(".txt") == -1:
+        nombreArchivoDestino += ".txt"
+else:
+    nombreArchivoDestino = "output.txt"
+
 etiquetas = ['MatCodig:  ', 'MatNumSe:  ', 'MatNumMe:  ', 'MatNumCo:  ', 'MatCant:  ', 'MatTipo:  ', 'MatDescr:  ', 'MatMarca:  ', 'MatLocal:  ', '$UpdatedBy:  ', '$Revisions:  ']
 # Cambiar estas etiquetas por las pertinentes según lo hablado con Merche
 alias = ['Et1:','Et2:','Et3:','Et4:','Et5:','Et6:','Et7','Et8:','Et9:','Et10:','Et11:']
 
 def crearArchivo(nombre):
     nombre += ".txt"
-    archivoDestino = open(nombre, 'w')
+    archivoDestino = open(nombreArchivoDestino, 'w')
     archivoDestino.write("## File successfully created!\n\n")
+    archivoDestino.write("## Formato: ")
+    texto = ""
+    for etiqueta in etiquetas:
+        texto = texto + etiqueta + ";"
+    texto += "\n\n"
+    archivoDestino.write(texto)
     archivoDestino.close()
 def escribirLinea(linea):
     linea += "\n"
-    archivoDestino = open(nombreArhivo + '.txt','a')
+    archivoDestino = open(nombreArchivoDestino,'a')
     archivoDestino.write(linea)
     archivoDestino.close()
-nombreArhivo = "prueba"
-crearArchivo(nombreArhivo)
-
-
+crearArchivo(nombreArchivo)
 archivoOrigen = open(nombreArchivo, 'r')
 for line in archivoOrigen:
     resultado = ""
